e6f791770077607adce5c418bf3c444e00539836
RedyAu <fodor.benedek2001@gmail.com>
2021-03-01 16:09:52 +0100

    format

--- Prog1/4/main.c
+++ Prog1/4/main.c
@@ -3,32 +3,39 @@
 
 void feltolt(unsigned char *szoveg);
 
-int main() {
+int main()
+{
     unsigned char szoveg[30] = {};
     feltolt(szoveg);
     return 0;
 }
 
 //Átkonvertálás kisbetűkké, csak betűk bekérése
-void feltolt(unsigned char *szoveg) {
+void feltolt(unsigned char *szoveg)
+{
     unsigned char c = 0, index = 0;
-    while (1) {
+    while (1)
+    {
         c = getche();
         //Ha enter, termináljuk a stringet
-        if (c == 13) {
+        if (c == 13)
+        {
             *(szoveg + index) = 0;
             break;
         }
         //Backspace
-        if (c == 8 && index > 0) *(szoveg + (index--)) = 0;
+        if (c == 8 && index > 0)
+            *(szoveg + (index--)) = 0;
         //Ha nem betű, újat kérünk be
-        if (!((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == ' ')) continue;
+        if (!((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == ' '))
+            continue;
         //Mentjük a karaktert
         *(szoveg + index) = (c >= 'A' && c <= 'Z') ? c + ('a' - 'A') : c;
 
         index++;
 
-        if (index == 29) {
+        if (index == 29)
+        {
             *(szoveg + index) = 0;
             break;
         }
