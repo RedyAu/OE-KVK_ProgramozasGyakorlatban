8789f79b9666c0d74f00479581e729308530664f
RedyAu <fodor.benedek2001@gmail.com>
2021-03-20 22:37:14 +0100

    control flow plan

--- Prog1/_beadando/main.c
+++ Prog1/_beadando/main.c
@@ -9,35 +9,38 @@
 #include <io.h>
 #include <fcntl.h>
 
-static int set_wide_stream(FILE *stream)
+static int consoleWide(FILE *stream)
 {
     return _setmode(_fileno(stream), _O_U16TEXT);
 }
 
 #else
 
-static int set_wide_stream(FILE *stream)
+static int consileWide(FILE *stream)
 {
     return fwide(stream, 1);
+    
 }
 
 #endif
 
 void init() {
     setlocale(LC_ALL, "");
-    set_wide_stream(stdout);
 }
 
 //!--------------------
 
 unsigned char game[9][9] = {0};
 
+unsigned char currentPlayer = 0;
+unsigned char currentGlobal = 0;
+
 int main() {
     init();
-    printf("%d, %d", game[0][0], game[1], [5]);
+
+    consoleWide(stdout);
+    wprintf(L"HELLO1 %d, %d\n", game[0][0], game[1][5]);
+
+    scanf(" ");
     return 0;
-}
-
-void setBig(char type) {
-
 }
--- Prog1/_beadando/terv.md
+++ Prog1/_beadando/terv.md
@@ -88,3 +88,19 @@ Alternate version without inner lines:
 ### Structure:
 Two dimensional array, first dimension global, second dimension local board.\
 `game[9][9];`
+
+### Control flow:
+#### Variable for current player:
+ - `0` = X
+ - `1` = 0
+
+#### Variable for selected global board:
+`0`-`8` : Selected board
+`9` : Unknown (game start or invalid), ask player next time
+
+ - Game begins. Initial conditions: Player is X, Selected board is 9 (unknown)
+ - X can move. Unknown global board, so X has to choose a local board.
+ - Local board validation, then X chooses field to place tick.
+ - Validation, then setting local to equivalent as field.
+ - O can move. Validation of selected global board, if right, O places a mark.
+ - etc.
